885d4a39aeb33712292291695fdc4fdc5d132290
Siddhant Sarkar <siddhant.gecbilaspur@gmail.com>
2019-10-26 00:50:12 +0530

    Update train.py

--- train.py
+++ train.py
@@ -28,7 +28,7 @@ parser.add_argument('--dataset', metavar='PATH', type=str, required=True, help='
 parser.add_argument('--model_name', metavar='MODEL', type=str, default='117M', help='Pretrained model name')
 parser.add_argument('--combine', metavar='CHARS', type=int, default=50000, help='Concatenate input files with <|endoftext|> separator into chunks of this minimum size')
 parser.add_argument('--encoding', type=str, default='utf-8', help='Set the encoding for reading and writing files.')
-
+parser.add_argument('--steps',type=int,default=1000,help='Number of Fine-tune steps.')
 parser.add_argument('--batch_size', metavar='SIZE', type=int, default=1, help='Batch size')
 parser.add_argument('--learning_rate', metavar='LR', type=float, default=0.00002, help='Learning rate for Adam')
 parser.add_argument('--accumulate_gradients', metavar='N', type=int, default=1, help='Accumulate gradients across N minibatches.')
@@ -255,7 +255,7 @@ def main():
         start_time = time.time()
 
         try:
-            while True:
+            while counter < args.steps :
                 if counter % args.save_every == 0:
                     save()
                 if counter % args.sample_every == 0:
